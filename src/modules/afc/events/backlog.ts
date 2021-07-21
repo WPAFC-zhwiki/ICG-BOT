d2af945dda443ca01a43d9352cc69aed67d84510
sunny00217wm <sunny00217wm@gmail.com>
2021-07-21 09:16:19 +0800

    fix CronJob

--- src/modules/afc/events/backlog.ts
+++ src/modules/afc/events/backlog.ts
@@ -2,7 +2,7 @@ import { CronJob } from 'cron';
 import { getBacklogInfo, send } from 'modules/afc/util';
 import winston from 'winston';
 
-const backlogCronJob = new CronJob( '0 0 */6 * * *', async function () {
+const backlogCronJob = new CronJob( '0 0 */4 * * *', async function () {
 	try {
 		const { tMsg, dMsg, iMsg, cnt, lvl } = await getBacklogInfo();
 
