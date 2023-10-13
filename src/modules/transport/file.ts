5624bb1258534195fb4f524c2bceea07af8c4148
sunafterrainwm <sunafterrainwm@gmail.com>
2023-10-13 13:26:15 +0800

    Update file.ts

--- src/modules/transport/file.ts
+++ src/modules/transport/file.ts
@@ -46,7 +46,8 @@ const USERAGENT = `AFC-ICG-BOT/${ version } (${ repository.replace( /^git\+/, ''
  * @return {string} 新文件名
  */
 function generateFileName( url: string, name: string ): string {
-	let extName: string = path.extname( name || '' );
+	// https://url/file.png?abc=123#456
+	let extName: string = path.extname( ( name || '' ).split( /[\?#]/ )[ 0 ] );
 	if ( extName === '' ) {
 		extName = path.extname( url || '' );
 	}
