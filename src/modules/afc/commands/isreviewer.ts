e3aa37d37518192806dbe56f6766096eafe93a5e
sunafterrainwm <sunafterrainwm@gmail.com>
2024-03-04 18:09:02 +0800

    add ex officio reviewer

--- src/modules/afc/commands/isreviewer.ts
+++ src/modules/afc/commands/isreviewer.ts
@@ -1,7 +1,7 @@
 import Discord = require( 'discord.js' );
 import winston = require( 'winston' );
 
-import { rebuildReviewerCaches, hasReviewerCaches, isReviewer, encodeURI, turndown, htmlToIRC, setCommand } from '@app/modules/afc/util';
+import { hasReviewersCache, isReviewer, encodeURI, turndown, htmlToIRC, setCommand, isExOfficioReviewer, isBlockedExOfficioReviewer, isParticipatingReviewer } from '@app/modules/afc/util';
 
 function upperFirst( str: string ) {
 	return str.slice( 0, 1 ).toUpperCase() + str.slice( 1, str.length );
@@ -27,9 +27,25 @@ setCommand( 'isreviewer', async function ( args, reply, bridgemsg ) {
 		return;
 	}
 
-	if ( hasReviewerCaches() ) {
-		winston.debug( `[afc/command/isreviewer] user: ${ user }, isreviewer: ${ isReviewer( user ) }` );
-		const msg = `使用者<a href="https://zh.wikipedia.org/wiki/User:${ encodeURI( user ) }">${ user }</a>${ isReviewer( user ) ? '是' : '不是' }審核員。`;
+	if ( hasReviewersCache() ) {
+		const isExOfficio = isExOfficioReviewer( user );
+		const isExOfficioBlocked = isExOfficio && isBlockedExOfficioReviewer( user );
+		const isParticipating = isParticipatingReviewer( user );
+		winston.debug( `[afc/command/isreviewer] user: ${ user }, isExOfficio: ${ isExOfficio }, isExOfficioBlocked: ${ isExOfficioBlocked }, isParticipating: ${ isParticipating }` );
+		let status = '<b>不是</b>審核員';
+		if ( isExOfficio ) {
+			if ( isExOfficioBlocked ) {
+				status = '是<b>被列入黑名單</b>的當然審核員';
+			} else {
+				status = '是<b>當然審核員</b>';
+			}
+			if ( isParticipating ) {
+				status += '，同時也是<b>參與審核員</b>';
+			}
+		} else if ( isParticipating ) {
+			status = '是<b>參與審核員</b>';
+		}
+		const msg = `使用者<a href="https://zh.wikipedia.org/wiki/User:${ encodeURI( user ) }">${ user }</a>${ status }。`;
 		reply( {
 			dMsg: new Discord.EmbedBuilder( {
 				description: turndown( msg ),
@@ -45,6 +61,5 @@ setCommand( 'isreviewer', async function ( args, reply, bridgemsg ) {
 			tMsg: '緩存無資料，請稍後再試。',
 			iMsg: '緩存無資料，請稍後再試。'
 		} );
-		rebuildReviewerCaches();
 	}
 } );
--- src/modules/afc/util/reviewer.ts
+++ src/modules/afc/util/reviewer.ts
@@ -1,46 +1,175 @@
+import { CronJob } from 'cron';
+import { MwnTitle } from 'mwn';
+import fetch, { Response } from 'node-fetch';
 import winston = require( 'winston' );
+
+import { inspect } from '@app/lib/util';
 
 import { mwbot } from '@app/modules/afc/util/index';
 import { recentChange, RecentChangeEvent } from '@app/modules/afc/util/recentchange';
 
-let caches: string[] = [];
+import { send } from './msg';
+import { escapeHTML } from './telegram-html';
 
-export async function rebuildReviewerCaches(): Promise<void> {
-	const data = await mwbot.request( {
-		action: 'query',
-		format: 'json',
-		prop: 'links',
-		titles: 'WikiProject:建立條目/參與者',
-		plnamespace: 2,
-		pllimit: 500
-	} );
+export let exOfficioReviewersCache: string[] = [];
+export let blockedExOfficioReviewersCaches: string[] = [];
 
-	const links: {
+export let participatingReviewersCache: string[] = [];
+
+interface ListSysopPatrollerApiResultV1 {
+	status: number;
+	data: string[];
+}
+
+interface ListSysopPatrollerApiError {
+	status: number;
+	error: string;
+}
+
+function normalizeUserName( user: string ) {
+	user = user.normalize();
+	let title: MwnTitle;
+	try {
+		title = new mwbot.Title( user, 2 );
+	} catch ( error ) {
+		return false;
+	}
+	if ( title.namespace !== 2 ) {
+		return false;
+	}
+	return title.getMainText();
+}
+
+export async function rebuildExOfficioReviewersCache(): Promise<void> {
+	try {
+		exOfficioReviewersCache = [];
+		await fetch( 'https://zhwp-afc-bot.toolforge.org/api/list-sysop-patroller?apiversion=2024v01' )
+			.then( async function ( res ): Promise<[ Response, string ]> {
+				return [ res, await res.text() ];
+			} )
+			.then( function ( [ res, resJsonTxt ] ) {
+				let resJson: ListSysopPatrollerApiError | ListSysopPatrollerApiResultV1;
+				try {
+					resJson = JSON.parse( resJsonTxt ) as ListSysopPatrollerApiError | ListSysopPatrollerApiResultV1;
+				} catch {
+					throw new Error( `Request fail, status: ${ res.status }, response: ${ resJsonTxt }` );
+				}
+				if ( resJson.status !== 200 ) {
+					throw new Error( resJsonTxt );
+				}
+				exOfficioReviewersCache = ( resJson as ListSysopPatrollerApiResultV1 ).data;
+			} );
+
+		winston.info( `[afc/util/reviewer] rebuild ex officio reviewer caches, length: ${ exOfficioReviewersCache.length }` );
+	} catch ( error ) {
+		winston.error( `[afc/util/reviewer] rebuild ex officio reviewer caches fail: ${ inspect( error ) }` );
+		send( {
+			tMsg: `rebuildExOfficioReviewersCache 失敗：${ escapeHTML( String( error ) ) }。 @sunafterrainwm`
+		}, 'debug' );
+	}
+}
+
+export async function rebuildBlockedExOfficioReviewersCache(): Promise<void> {
+	try {
+		const data = await mwbot.request( {
+			action: 'query',
+			format: 'json',
+			prop: 'links',
+			titles: 'WikiProject:建立條目/參與者/黑名單',
+			plnamespace: 2,
+			pllimit: 500
+		} );
+
+		const links: {
+			ns: number;
+			title: string;
+		}[] = data.query.pages[ 0 ].links;
+
+		blockedExOfficioReviewersCaches = [];
+
+		// 黑名單可能是空的
+		// 這時就沒東西
+		if ( links ) {
+			links.filter( function ( link ) {
+				return link.ns === 2;
+			} ).forEach( function ( link ) {
+				blockedExOfficioReviewersCaches.push( normalizeUserName( link.title ) as string );
+			} );
+		}
+
+		winston.info( `[afc/util/reviewer] rebuild ex officio reviewer blocked caches, length: ${ blockedExOfficioReviewersCaches.length }` );
+	} catch ( error ) {
+		winston.error( `[afc/util/reviewer] rebuild ex officio reviewer blocked caches fail: ${ inspect( error ) }` );
+		send( {
+			tMsg: `rebuildBlockedExOfficioReviewersCache 失敗：${ escapeHTML( String( error ) ) }。 @sunafterrainwm`
+		}, 'debug' );
+	}
+}
+
+export async function rebuildParticipatingReviewersCache(): Promise<void> {
+	try {
+		const data = await mwbot.request( {
+			action: 'query',
+			format: 'json',
+			prop: 'links',
+			titles: 'WikiProject:建立條目/參與者',
+			plnamespace: 2,
+			pllimit: 500
+		} );
+
+		const links: {
 		ns: number;
 		title: string;
 	}[] = data.query.pages[ 0 ].links;
 
-	caches = [];
+		participatingReviewersCache = [];
 
-	links.filter( function ( link ) {
-		return link.ns === 2;
-	} ).forEach( function ( link ) {
-		caches.push( link.title );
-	} );
+		links.filter( function ( link ) {
+			return link.ns === 2;
+		} ).forEach( function ( link ) {
+			participatingReviewersCache.push( normalizeUserName( link.title ) as string );
+		} );
 
-	winston.debug( `[afc/util/reviewer] rebuild reviewer caches, length: ${ caches.length }` );
-}
-
-function upperFirst( str: string ) {
-	return str.slice( 0, 1 ).toUpperCase() + str.slice( 1, str.length );
+		winston.info( `[afc/util/reviewer] rebuild participating reviewer caches, length: ${ participatingReviewersCache.length }` );
+	} catch ( error ) {
+		winston.error( `[afc/util/reviewer] rebuild participating reviewer caches fail: ${ inspect( error ) }` );
+		send( {
+			tMsg: `rebuildParticipatingReviewersCache 失敗：${ escapeHTML( String( error ) ) }。 @sunafterrainwm`
+		}, 'debug' );
+	}
 }
 
 export function isReviewer( user: string ): boolean {
-	return caches.includes( `User:${ upperFirst( user ) }` );
+	return isParticipatingReviewer( user ) || ( isExOfficioReviewer( user ) && !isBlockedExOfficioReviewer( user ) );
 }
 
-export function hasReviewerCaches(): boolean {
-	return !!caches.length;
+export function isExOfficioReviewer( user: string ): boolean {
+	const userName = normalizeUserName( user );
+	if ( !userName ) {
+		return false;
+	}
+
+	return exOfficioReviewersCache.includes( userName );
+}
+
+export function isBlockedExOfficioReviewer( user: string ): boolean {
+	const userName = normalizeUserName( user );
+	if ( !userName ) {
+		return false;
+	}
+	return blockedExOfficioReviewersCaches.includes( userName );
+}
+
+export function isParticipatingReviewer( user: string ): boolean {
+	const userName = normalizeUserName( user );
+	if ( !userName ) {
+		return false;
+	}
+	return participatingReviewersCache.includes( userName );
+}
+
+export function hasReviewersCache(): boolean {
+	return !!participatingReviewersCache.length && !!exOfficioReviewersCache.length;
 }
 
 recentChange.addProcessFunction( function ( event: RecentChangeEvent ): boolean {
@@ -53,7 +182,26 @@ recentChange.addProcessFunction( function ( event: RecentChangeEvent ): boolean 
 
 	return false;
 }, function (): void {
-	rebuildReviewerCaches();
+	rebuildParticipatingReviewersCache();
 } );
 
-rebuildReviewerCaches();
+recentChange.addProcessFunction( function ( event: RecentChangeEvent ): boolean {
+	if (
+		event.type === 'edit' &&
+		event.title === 'WikiProject:建立條目/參與者/黑名單'
+	) {
+		return true;
+	}
+
+	return false;
+}, function (): void {
+	rebuildExOfficioReviewersCache();
+} );
+
+new CronJob( '0 35 * * * *', function () {
+	rebuildExOfficioReviewersCache();
+} ).start();
+
+rebuildExOfficioReviewersCache();
+rebuildBlockedExOfficioReviewersCache();
+rebuildParticipatingReviewersCache();
