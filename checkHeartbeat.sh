88f51de9252a930f6e83e8d577c0250e2bb0a8b6
sunafterrainwm <sunafterrainwm@gmail.com>
2024-05-22 02:05:16 +0800

    =

--- checkHeartbeat.sh
+++ checkHeartbeat.sh
@@ -3,11 +3,17 @@
 # https://stackoverflow.com/questions/59895#answer-53183593
 PROGRAM_ROOT="$( realpath "$( dirname "${BASH_SOURCE[0]}" )" )"
 CONFIG_ROOT="$PROGRAM_ROOT/config"
+HEART_CONFIG="$CONFIG_ROOT/heartbeatConfig.sh"
 
-source "$CONFIG_ROOT/heartbeatConfig.sh"
+if [ ! -f "$HEART_CONFIG" ]; then
+    # config file not found
+    exit 1
+fi
+
+source "$HEART_CONFIG"
 
 if [ ! -f "$STATUS_FILE" ]; then
-    # Status file not found
+    # status file not found
     exit 1
 fi
 
