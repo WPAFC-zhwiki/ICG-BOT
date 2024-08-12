89b0797619bd420c40a32021ba49aa3d7c645b50
sunafterrainwm <sunafterrainwm@gmail.com>
2024-08-12 06:42:41 +0000

    =

--- checkHeartbeat.sh
+++ checkHeartbeat.sh
@@ -20,11 +20,11 @@ fi
 last_update=$( stat -c %Y "$STATUS_FILE" )
 current_time=$( date +%s )
 no_update_time=$(( current_time - last_update ))
-formatted_current_time=$( date '+%Y-%m-%dT%H:%M:%SZ' --utc "--date=@$current_time" )
-formatted_no_update_time=$( date '+%Y-%m-%dT%H:%M:%SZ' --utc "--date=@$no_update_time" )
+formatted_current=$( date '+%Y-%m-%dT%H:%M:%SZ' --utc "--date=@$current_time" )
+formatted_last_update=$( date '+%Y-%m-%dT%H:%M:%SZ' --utc "--date=@$last_update" )
 log_file="$PROGRAM_ROOT/logs/heartbeat.log"
 
-echo "[$formatted_current_time] Last update was at $formatted_no_update_time, which is $no_update_time seconds from now." | tee -a "$log_file"
+echo "[$formatted_current] Last update was at $formatted_last_update, which is $no_update_time seconds from now." | tee -a "$log_file"
 tail -n 1000 "$log_file" > "$log_file-tmp" && mv "$log_file-tmp" "$log_file"
 
 if [ "$no_update_time" -gt "$MAX_NO_UPDATE_TIME" ]; then
